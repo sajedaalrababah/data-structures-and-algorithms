8b84837e157f3002e667fc6d67b2241b0fec7038
sajedaalrababah <sajedaalrababah@icloud.com>
2023-05-14 22:59:47 +0300

    cc6

--- linked-list/link_list.py
+++ linked-list/link_list.py
@@ -1,39 +1,91 @@
-class Node : 
-    def __init__(self, value,_next=None):
+class Node:
+    def __init__(self, value):
         self.value = value
-        self.next = _next
-
+        self.next = None
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
 
-    def insert(self, data):
-        
-        newNode = Node(data)
-        newNode.next = self.head
-        self.head = newNode    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
 
+    def insert_before(self, value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        if self.head.value == value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
 
+    def insert_after(self, value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
 
-    def includes(self,data):
-        
-        current = self.head
-        while current is not None:
-            if current.value == data:
+    def includes(self, value):
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
                 return True
-            current = current.next
-        return False    
-    
+            current_node = current_node.next
+        return False
 
     def to_string(self):
-        current = self.head
-        string = ""
-        while current is not None:
-            string += f"{{ {current.value} }} -> "
-            current = current.next
-        string += "NULL"
-        return string
+        current_node = self.head
+        result = ""
+        while current_node:
+            result += "{ " + str(current_node.value) + " } -> "
+            current_node = current_node.next
+        result += "NULL"
+        return result
+
+    def delete(self, value):
+        current_node = self.head
+        if current_node and current_node.value == value:
+            self.head = current_node.next
+            current_node = None
+            return
+
+        previous_node = None
+        while current_node and current_node.value != value:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        previous_node.next = current_node.next
+        current_node = None
+
+        
--- linked-list/tests/test_linkedList.py
+++ linked-list/tests/test_linkedList.py
@@ -42,3 +42,57 @@ def test_to_string():
     linked_list.insert(10)
     linked_list.insert(15)
     assert linked_list.to_string() == "{ 15 } -> { 10 } -> { 5 } -> NULL"
+
+def test_append_linked_list():
+    linked_list = LinkedList()
+    linked_list.append(5)
+    assert linked_list.head.value == 5
+
+
+def test_append_multiple_linked_list():
+    linked_list = LinkedList()
+    linked_list.append(5)
+    linked_list.append(10)
+    linked_list.append(15)
+    assert linked_list.head.value == 5
+    assert linked_list.head.next.value == 10
+    assert linked_list.head.next.next.value == 15
+
+
+def test_insert_before_linked_list_middle():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_before(10, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 20 } -> { 10 } -> { 5 } -> NULL"
+
+def test_insert_before_linked_list_first():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_before(15, 20)
+    assert linked_list.to_string(
+    ) == "{ 20 } -> { 15 } -> { 10 } -> { 5 } -> NULL"
+
+
+def test_insert_after_linked_list_middle():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_after(10, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 10 } -> { 20 } -> { 5 } -> NULL"
+
+
+def test_insert_after_linked_list_last():
+    linked_list = LinkedList()
+    linked_list.insert(5)
+    linked_list.insert(10)
+    linked_list.insert(15)
+    linked_list.insert_after(5, 20)
+    assert linked_list.to_string(
+    ) == "{ 15 } -> { 10 } -> { 5 } -> { 20 } -> NULL"
