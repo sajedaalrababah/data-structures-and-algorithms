4bcd198b9b95408d4d08e880fc3b748205e1a69f
sajedaalrababah <sajedaalrababah@icloud.com>
2023-06-06 20:52:18 +0300

    fg

--- stack_queue_brackets/stack_queue_Brackets.py
+++ stack_queue_brackets/stack_queue_Brackets.py
@@ -1,20 +1,62 @@
+class Stack:
+    def __init__(self):
+        """
+        Initialize an empty stack.
+        """
+        self.stack = []
+
+    def push(self, item):
+        """
+        Push an item onto the top of the stack.
+
+        Args:
+            item: The item to be pushed onto the stack.
+        """
+        self.stack.append(item)
+
+    def pop(self):
+        """
+        Remove and return the item at the top of the stack.
+
+        Returns:
+            The item at the top of the stack, or None if the stack is empty.
+        """
+        if not self.is_empty():
+            return self.stack.pop()
+        return None
+
+    def is_empty(self):
+        """
+        Check if the stack is empty.
+
+        Returns:
+            True if the stack is empty, False otherwise.
+        """
+        return len(self.stack) == 0
+
+
 def validate_brackets(s):
-    stack = []
-    opening_brackets = ['(', '[', '{']
-    closing_brackets = [')', ']', '}']
-    bracket_pairs = {'(': ')', '[': ']', '{': '}'}
+    """
+    Check if the brackets in a string are balanced.
+
+    Args:
+        s (str): The input string containing brackets.
+
+    Returns:
+        bool: True if the brackets are balanced, False otherwise.
+    """
+    stack = Stack()
+    opening_brackets = ["(", "[", "{"]
+    closing_brackets = [")", "]", "}"]
+    brackets_map = {")": "(", "]": "[", "}": "{"}
 
     for char in s:
         if char in opening_brackets:
-            stack.append(char)
+            stack.push(char)
         elif char in closing_brackets:
-            if len(stack) == 0 or bracket_pairs[stack.pop()] != char:
+            if stack.is_empty():
+                return False
+            if brackets_map[char] != stack.pop():
                 return False
 
-    return len(stack) == 0
-
-
-print(validate_brackets("()"))  # True
-print(validate_brackets("({[]})"))  # True
-print(validate_brackets("({[})"))  # False
-print(validate_brackets("({)"))  # False
+    return stack.is_empty()
